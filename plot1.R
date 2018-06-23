660e01676cadfb3328a6d4436e8b846f1be3b4d9
marinjorian <jixiao2009.bio@gmail.com>
2018-06-23 19:09:08 -0400

    submit homework 2

--- plot1.R
+++ plot1.R
@@ -1,2 +1,9 @@
-setwd("C:\\Users\\xj713756\\Repos\\ExploratoryDataAnalysis")
+setwd("C:\\Users\\xj713756\\Repos\\ExData_Plotting1")
 dat = read.table("household_power_consumption.txt", header = T, sep=';', na.strings = '?')
+dat_twoday = dat[which(dat$Date %in% c("1/2/2007", "2/2/2007")),]
+#Generate plot1
+hist(dat_twoday$Global_active_power, col = 'red', main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, "plot1.png", width = 480, height = 480, units = "px")
+dev.off()
+
+
