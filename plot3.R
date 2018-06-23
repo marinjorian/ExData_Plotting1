d1012f1bf235553da5a607d1240f65077bf1d709
marinjorian <jixiao2009.bio@gmail.com>
2018-06-23 19:23:04 -0400

    submit homework 3

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+setwd("C:\\Users\\xj713756\\Repos\\ExData_Plotting1")
 dat = read.table("household_power_consumption.txt", header = T, sep=';', na.strings = '?')
 dat_twoday = dat[which(dat$Date %in% c("1/2/2007", "2/2/2007")),]
 
@@ -14,7 +15,7 @@ lines(dat_twoday$DateTime, dat_twoday$Sub_metering_2, col = "red")
 points(dat_twoday$DateTime, dat_twoday$Sub_metering_3, type = "n")
 lines(dat_twoday$DateTime, dat_twoday$Sub_metering_3, col = "blue")
 
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, cex = 1)
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "blue"), lty = 1, cex = 0.8)
 
 dev.copy(png, "plot3.png", width = 480, height = 480, units = "px")
 dev.off()
